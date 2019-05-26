e0b32946b574ee25b8ad589e8a294f8fe6beab55
Sukhdip Singh Nijjar <sukhdipsinghnijjar@dhcp-10-15-137-187.wireless.pacific.edu>
2019-05-26 12:19:50 -0700

    setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCUOMEy4BclAsKCNHyrn1PoIYlWFWebwlY",
-    authDomain: "expensify-92d12.firebaseapp.com",
-    databaseURL: "https://expensify-92d12.firebaseio.com",
-    projectId: "expensify-92d12",
-    storageBucket: "expensify-92d12.appspot.com",
-    messagingSenderId: "221610247676",
-    appId: "1:221610247676:web:0ee349604ba467e7"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
   };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
