33c16dc729c032e7c9dfb883e9f062070cce5cf2
Sukhdip Singh Nijjar <sukhdipsinghnijjar@dhcp-10-15-137-187.wireless.pacific.edu>
2019-05-28 15:36:28 -0700

    adding user authentication so they can login and logout also only once logging in can they see specific pages

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, {history} from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import './firebase/firebase';
+import {firebase} from './firebase/firebase';
 
 const store = configureStore();
 
@@ -21,7 +21,31 @@ const jsx = (
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+}
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user)
+  {
+    store.dispatch(login(user.uid));
+    console.log("logged in");
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
+  }
+  else {
+    store.dispatch(logout());
+    console.log("logged out");
+    renderApp();
+    history.push('/');
+  }
 })
 
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 import { shallow } from 'enzyme';
 
 // test('should render Header correctly', () => {
@@ -9,6 +9,13 @@ import { shallow } from 'enzyme';
 // });
 
 test('Should render Header correctly', () => {
-  const wrapper = shallow(<Header />);
+  const wrapper = shallow(<Header startLogout={() => {}}/>);
   expect(wrapper).toMatchSnapshot();
-})
+});
+
+test('Should call startLogout on button click', () => {
+  let startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
+});
