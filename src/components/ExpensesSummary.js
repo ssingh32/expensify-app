184a969d7d14d1ce5b629853a45c0bd718a5b150
Sukhdip Singh Nijjar <sukhdipsinghnijjar@dhcp-10-15-137-187.wireless.pacific.edu>
2019-06-03 20:32:26 -0700

    adding information box to expense dashboard when not all expenses shown

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -5,13 +5,19 @@ import { Link } from 'react-router-dom';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = ({expenseCount, expensesTotal}) => {
+export const ExpensesSummary = ({expenseCount, expensesTotal, nonVisibleExpenses}) => {
     const expenseWord = expenseCount === 1 ? 'expense': 'expenses';
+    const nonVisibleExpenseWord = nonVisibleExpenses <= 1 ? 'expense': 'expenses';
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,00.00');
    return (
     <div className={"page-header"}>
         <div className="content-container">
             <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+            {nonVisibleExpenses > 0 ? 
+                <p className="page-header__title">Not showing <span>{nonVisibleExpenses}</span> {nonVisibleExpenseWord} because of filters</p> 
+                :
+                <p className="page-header__title">Showing all {expenseWord} no filters in place</p>
+            }
             <div className="page-header__actions">
                 <Link className="button" to="create">Add Expense</Link>
             </div>
@@ -24,7 +30,8 @@ const mapStateToProps = (state) => {
     const visibleExpenses = selectExpenses(state.expenses, state.filters);
     return {
         expenseCount : visibleExpenses.length,
-        expensesTotal : selectExpensesTotal(visibleExpenses)
+        expensesTotal : selectExpensesTotal(visibleExpenses),
+        nonVisibleExpenses: state.expenses.length - visibleExpenses.length
     };
 };
 
